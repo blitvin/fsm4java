6eef27920a9dc506e2015e25fc946a80ed437c0d
blitvin <tankodrum@gmail.com>
2015-07-21 21:17:49 +0300

    Revert "Fix code that doesn"
    This reverts commit d29e8f22153bc92719f2b5562f3c10889db1a624.

--- src/org/blitvin/statemachine/StateMachineBuilder.java
+++ src/org/blitvin/statemachine/StateMachineBuilder.java
@@ -58,17 +58,14 @@ public class StateMachineBuilder<EventType extends Enum<EventType>> {
 	}
 	
 	
-	@SuppressWarnings({ "unchecked", "rawtypes" })
+	@SuppressWarnings("unchecked")
 	/**
 	 * This constructor assumes SimpleStateMachine class
 	 * @param name name string identifying state machine. Currently is not used, and provided for  ( factories provided by the package use
 	 * state machine names for referencing) 
 	 */
 	public StateMachineBuilder(String name){
-		this(name,(Class)SimpleStateMachine.class);
-		/* this(name,(Class<? extends StateMachine<EventType>>) SimpleStateMachine.class);
-		 * for some reason the above line doesn't compile in JDK1.8, even though it is OK in 1.6 and 1.7..
-		 */
+		this(name,(Class<? extends StateMachine<EventType>>) SimpleStateMachine.class);
 	}
 	
 	public StateMachineBuilder<EventType> addState(State<EventType> newState){
